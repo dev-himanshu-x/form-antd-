5557c9dfab5d9ae60ed9c848ba1f8d9a188725a6
Himanshu <iam.himanshu.x@gmail.com>
2026-02-01 14:20:04 +0530

    Form completed

---
+++ src/routes/details.tsx
@@ -0,0 +1,61 @@
+import { Button, Form, Input, Select, Space, InputNumber } from "antd";
+import { CloseOutlined } from "@ant-design/icons";
+
+function Mark({ field }: any) {
+  const marks = Form.useWatch(
+    ["academic detail", field.index, "marks"],
+    field.form,
+  );
+
+  return (
+    <Space key={field.subField.key}>
+      <Form.Item
+        label="Board"
+        rules={[{ required: true, message: "Field can not be empty" }]}
+        name={[field.subField.name, "Board"]}
+      >
+        <Input placeholder="Boards" type="text" />
+      </Form.Item>
+
+      <Form.Item
+        label="Marks Type"
+        rules={[{ required: true, message: "Field can not be empty" }]}
+        name={[field.subField.name, "marks"]}
+      >
+        <Select
+          placeholder="Marks Type"
+          options={[
+            { value: "CGPA", label: "CGPA" },
+            { value: "Percentage", label: "Percentage" },
+          ]}
+        />
+      </Form.Item>
+
+      {marks === "CGPA" ? (
+        <Form.Item
+          label="CGPA"
+          rules={[{ required: true, message: " Cgpa is required" }]}
+          name={[field.subField.name, "CGPA"]}
+        >
+          <InputNumber max={10} min={0} />
+        </Form.Item>
+      ) : marks === "Percentage" ? (
+        <Form.Item
+          label="Percentage"
+          rules={[{ required: true, message: "Percentage is required" }]}
+          name={[field.subField.name, "Percentage"]}
+        >
+          <InputNumber max={100} min={0} />
+        </Form.Item>
+      ) : null}
+
+      {field.subField.name !== 0 && (
+        <Button onClick={() => field.subOpt.remove(field.subField.name)}>
+          <CloseOutlined />
+        </Button>
+      )}
+    </Space>
+  );
+}
+
+export default Mark;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router";
 import "../App.css";
 import axios from "axios";
-
 export const Route = createFileRoute("/")({ component: App });
-
 import { useEffect, useState } from "react";
-import { Button, Form, Input, Select, Divider, Space, InputNumber } from "antd";
+import { Button, Form, Input, Select, Divider, Space, Row } from "antd";
 import type { DefaultOptionType } from "antd/es/select";
-import { CloseOutlined } from "@ant-design/icons";
+import Mark from "./details";
 
 function App() {
   const [options, setOptions] = useState<DefaultOptionType[]>([]);
   const [postOffices, setPostOffices] = useState<any[]>([]);
   const [btndisabled, setbtndisabled] = useState(true);
-
   const api = "https://api.postalpincode.in/pincode/";
   const [form] = Form.useForm();
   const { Search } = Input;
   const pincode = Form.useWatch(["address", "post_office", "pincode"], form);
-  const marks = Form.useWatch(["academic detail", 0, "marks"], form);
   var url = api + pincode;
+  const { TextArea } = Input;
 
   useEffect(() => {
     if (pincode && pincode.length === 6) {
@@ -32,11 +29,20 @@ function App() {
             })),
           );
           setbtndisabled(false);
+          form.setFieldsValue({
+            address: {
+              post_office: {
+                post_office: "",
+                state: "",
+                district: "",
+                city: "",
+              },
+            },
+          });
         }
       });
     }
   }, [pincode]);
-
   const change = (value: string) => {
     const selected = postOffices.find((po) => po.Name === value);
     if (selected) {
@@ -51,128 +57,100 @@ function App() {
       });
     }
   };
-
   const onFinish = (values: any) => {
     console.log(values);
+    form.resetFields();
   };
-
   return (
-    <div className="p-3">
-      <Form layout="inline" form={form} onFinish={onFinish}>
+    <div className="p-3 bg-[#eceeff] h-screen">
+      <Form
+        layout="inline"
+        form={form}
+        onFinish={onFinish}
+        style={{ display: "flex", flexWrap: "wrap" }}
+      >
         <Divider titlePlacement="start">Address</Divider>
-
         <Space>
-          <Form.Item
-            label="Pincode"
-            rules={[{ required: true }]}
-            name={["address", "post_office", "pincode"]}
-          >
-            <Search placeholder="Pincode" maxLength={6} type="number" />
-          </Form.Item>
-
-          <Form.Item
-            label="Post Office"
-            rules={[{ required: true }]}
-            name={["address", "post_office", "post_office"]}
-          >
-            <Select
-              placeholder="Post Office"
-              options={options}
-              disabled={btndisabled}
-              onChange={change}
-            />
-          </Form.Item>
-
-          <Form.Item label="State" name={["address", "post_office", "state"]}>
-            <Input disabled />
-          </Form.Item>
-
-          <Form.Item
-            label="District"
-            name={["address", "post_office", "district"]}
-          >
-            <Input disabled />
-          </Form.Item>
-
-          <Form.Item label="City" name={["address", "post_office", "city"]}>
-            <Input disabled />
-          </Form.Item>
+          <Row gutter={[6, 20]}>
+            <Form.Item
+              label="Pincode"
+              rules={[{ required: true, message: "Field can not be empty" }]}
+              name={["address", "post_office", "pincode"]}
+            >
+              <Search
+                placeholder="Pincode"
+                maxLength={6}
+                type="number"
+                style={{ minWidth: "auto", maxWidth: "12.5rem" }}
+              />
+            </Form.Item>
+            <Form.Item
+              label="Post Office"
+              rules={[{ required: true, message: "Field can not be empty" }]}
+              name={["address", "post_office", "post_office"]}
+            >
+              <Select
+                style={{ width: 140 }}
+                options={options}
+                disabled={btndisabled}
+                onChange={change}
+              />
+            </Form.Item>
+            <Form.Item label="State" name={["address", "post_office", "state"]}>
+              <Input disabled />
+            </Form.Item>
+            <Form.Item
+              label="District"
+              name={["address", "post_office", "district"]}
+            >
+              <Input disabled />
+            </Form.Item>
+            <Form.Item label="City" name={["address", "post_office", "city"]}>
+              <Input disabled />
+            </Form.Item>
+            <Form.Item
+              label="Address"
+              rules={[{ required: true, message: "Field can not be empty" }]}
+              name={["address", "post_office", "address"]}
+            >
+              <TextArea
+                style={{ height: 80, resize: "none" }}
+                placeholder="Home Address"
+              />
+            </Form.Item>
+          </Row>
         </Space>
-
-        <Form.Item
-          label="Address"
-          rules={[{ required: true }]}
-          name={["address", "post_office", "address"]}
-        >
-          <Input />
-        </Form.Item>
-
         <Divider titlePlacement="start">Academic Details</Divider>
-
         <Form.Item>
           <Form.List name={["academic detail"]} initialValue={[{}]}>
-            {(subFields, subOpt) => (
-              <div
-                style={{ display: "flex", flexDirection: "column", rowGap: 16 }}
-              >
-                {subFields.map((subField) => (
-                  <Space key={subField.key}>
-                    <Form.Item
-                      label="Board"
-                      rules={[{ required: true }]}
-                      name={[subField.name, "Board"]}
-                    >
-                      <Input />
-                    </Form.Item>
-
-                    <Form.Item
-                      label="Marks Type"
-                      rules={[{ required: true }]}
-                      name={[subField.name, "marks"]}
-                    >
-                      <Select
-                        placeholder="Marks Type"
-                        options={[
-                          { value: "CGPA", label: "CGPA" },
-                          { value: "Percentage", label: "Percentage" },
-                        ]}
-                      />
-                    </Form.Item>
-
-                    {marks === "CGPA" ? (
-                      <Form.Item
-                        label="CGPA"
-                        rules={[{ required: true }]}
-                        name={[subField.name, "CGPA"]}
-                      >
-                        <InputNumber max={10} min={0} />
-                      </Form.Item>
-                    ) : marks === "Percentage" ? (
-                      <Form.Item
-                        label="Percentage"
-                        rules={[{ required: true }]}
-                        name={[subField.name, "Percentage"]}
-                      >
-                        <InputNumber max={100} min={0} />
-                      </Form.Item>
-                    ) : null}
-
-                    {subField.name !== 0 && (
-                      <Button onClick={() => subOpt.remove(subField.name)}>
-                        <CloseOutlined />
-                      </Button>
-                    )}
-                  </Space>
-                ))}
-
-                <Button type="dashed" onClick={() => subOpt.add()} block>
-                  + Add More
-                </Button>
-              </div>
-            )}
+            {(subFields, subOpt) => {
+              return (
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    rowGap: 16,
+                  }}
+                >
+                  {subFields.map((subField, index) => (
+                    <Mark
+                      field={{
+                        subField: subField,
+                        index: index,
+                        subOpt: subOpt,
+                        form: { form },
+                      }}
+                    />
+                  ))}
+                  <Button type="dashed" onClick={() => subOpt.add()} block>
+                    + Add More
+                  </Button>
+                </div>
+              );
+            }}
           </Form.List>
         </Form.Item>
-
+        <Divider titlePlacement="start">Submit</Divider>
         <Button htmlType="submit" className="w-full">
           Submit
         </Button>
@@ -180,5 +158,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
